a8d61a88f1fd47681e431b57552d25286dd11737
Allan Jardine <allan@datatables.net>
2022-10-27 15:15:40 +0000

    Update typing information to latest typing integration for DataTables

---
+++ types/types.d.ts
@@ -0,0 +1,42 @@
+
+// Dist-DataTables-StateRestore-jQueryUI integration with jQueryUI exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-StateRestore-jQueryUI integration with jQueryUI exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-StateRestore-jQueryUI integration with jQueryUI exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-StateRestore-jQueryUI integration with jQueryUI exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-StateRestore-jQueryUI integration with jQueryUI exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-StateRestore-jQueryUI integration with jQueryUI exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
